3c539c847b5ad889bf0ba319719badd37f2579c8
“Pei <peidailylife@gmail.com>
2018-09-02 14:32:39 -0500

    Deploy

---
+++ ddstockpredict/web.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def navigate():
+#     return render_template('layout.html')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/features/')
+def features():
+    return render_template('features.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
